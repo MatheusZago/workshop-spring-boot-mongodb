5a324bee73d505c2815da0e8ac7590b53840829a
Matheus Lui <matheuszago134@gmail.com>
2024-09-05 17:58:25 -0300

    Projecting fields of author with DTO

--- src/main/java/com/nelioalves/workshopmongo/domain/Post.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/Post.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import com.nelioalves.workshopmongo.dto.AuthorDTO;
+
 public class Post implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -12,13 +14,13 @@ public class Post implements Serializable{
 	private Date date;
 	private String title;
 	private String body;
-	private User author; //Pela relaçã de dependencia
+	private AuthorDTO author; //Pela relaçã de dependencia
 	
 	public Post() {
 		
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO  author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -59,11 +61,11 @@ public class Post implements Serializable{
 		this.body = body;
 	}
 
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
