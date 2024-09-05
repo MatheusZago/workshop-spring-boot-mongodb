bcbe818bedfe73c9a4be60e2871f561071d3c506
Matheus Lui <matheuszago134@gmail.com>
2024-09-05 19:02:33 -0300

    Simple consult with @Query

--- src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.nelioalves.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.nelioalves.workshopmongo.domain.Post;
@@ -12,5 +13,12 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	
 	//Usando query method, só de criar o nome de certa forma ela monta uma consulta automaticamente
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	
+	//Isso aqui é pra executar  query automatica de antes, só que de forma mais personalizada.
+	//Title ta dizendo qual campo vai buscar, ?0 quer dizer que ele vai pegar o primeiro parametro do metodo pra usar
+	//E i no options quer dizer q ignora case
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 }
