0a8316f8fd96921e3f2ab23514ce748e4642b6f8
Matheus Lui <matheuszago134@gmail.com>
2024-09-05 18:33:22 -0300

    Add comments to posts

--- src/main/java/com/nelioalves/workshopmongo/domain/Post.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/Post.java
@@ -1,10 +1,13 @@
 package com.nelioalves.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import com.nelioalves.workshopmongo.dto.AuthorDTO;
+import com.nelioalves.workshopmongo.dto.CommentDTO;
 
 public class Post implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -15,6 +18,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author; //Pela relaçã de dependencia
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -67,6 +72,14 @@ public class Post implements Serializable{
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
