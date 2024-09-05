26e11e06df2b15c8742aeced2d89569007962eb8
Matheus Lui <matheuszago134@gmail.com>
2024-09-05 15:55:20 -0300

    Using DTO pattern to return users

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResources.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResources.java
@@ -1,6 +1,7 @@
 package com.nelioalves.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.dto.UserDTO;
 import com.nelioalves.workshopmongo.services.UserService;
 
 @RestController //Está dizendo que é uma classe controladora e que os métodos dela devem voltar em Json
@@ -20,9 +22,12 @@ public class UserResources {
 
 //	@RequestMapping(method = RequestMethod.GET) //Faz a msm coisa do de baixo
 	@GetMapping //Para falar que ele puxa esse método com um get
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		//Já ta criando a lista com os valores do findAll do service
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		
+		//Transformando os usuarios em usuários DTO para que seus dados sejam melhor acessaados
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
